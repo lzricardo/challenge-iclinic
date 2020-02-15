078722d25867da5d72e79a9f1ccbc07c4eac6ccc
lzricardo <lzricardo.ecomp@gmail.com>
2020-02-15 18:56:41 -0300

    :sparkles: Create patient service.

---
+++ src/services/patient.js
@@ -0,0 +1,43 @@
+const cacheRequest = require('../config/cache-request');
+const rp = require('request-plus');
+
+class PatientService {
+    constructor() {
+        this.request = rp(({
+            defaults: {
+                baseUrl: process.env.PATIENTS_API_URI,
+                headers: {
+                    'Accept': 'Accept: application/json',
+                    'Authorization': `Bearer ${process.env.PATIENTS_API_TOKEN}`
+                },
+                rejectUnauthorized: (process.env.APP_ENV !== 'development'),
+                timeout: process.env.PATIENTS_API_TIMEOUT,
+                retry: {
+                    attempts: process.env.PATIENTS_API_RETRIES,
+                },
+                cache: {
+                    cache: cacheRequest,
+                    cacheOptions: {
+                        ttl: process.env.PATIENTS_API_TTL
+                    }
+                }
+            }
+        }));
+    }
+
+    getById(id) {
+        return new Promise((resolve, reject) => {
+            this.request({
+                url: `patients/${id}`
+            })
+                .then(data => {
+                    resolve(data);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    }
+}
+
+module.exports = new PatientService;
